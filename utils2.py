abe2114fe164e41f1fb9b280a83b666dbb85c857
Jeremy Howard <j@fast.ai>
2017-03-13 14:57:34 -0700

    updates

--- utils2.py
+++ utils2.py
@@ -109,3 +109,34 @@ def fit_gen(gen, fn, eval_fn, nb_iter):
         fn(*next(gen))
         if i % (nb_iter//10) == 0: eval_fn()
 
+
+def wrap_config(layer):
+    return {'class_name': layer.__class__.__name__, 'config': layer.get_config()}
+
+
+def copy_layer(layer): return layer_from_config(wrap_config(layer))
+
+
+def copy_layers(layers): return [copy_layer(layer) for layer in layers]
+
+
+def copy_weights(from_layers, to_layers):
+    for from_layer,to_layer in zip(from_layers, to_layers):
+        to_layer.set_weights(from_layer.get_weights())
+
+
+def copy_model(m):
+    res = Sequential(copy_layers(m.layers))
+    copy_weights(m.layers, res.layers)
+    return res
+
+
+def insert_layer(model, new_layer, index):
+    res = Sequential()
+    for i,layer in enumerate(model.layers):
+        if i==index: res.add(new_layer)
+        copied = layer_from_config(wrap_config(layer))
+        res.add(copied)
+        copied.set_weights(layer.get_weights())
+    return res
+
